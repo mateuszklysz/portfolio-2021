a9020e33f2463c0d8372fc50236e368bf78d76e7
Mateusz Kłysz <mateuszklysz@outlook.com>
2020-09-08 15:03:28 +0200

    fix preload black cube

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import gsap from "gsap";
@@ -10,6 +10,7 @@ import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 
 const StyledContainer = styled.section`
+  opacity: 0;
   position: fixed;
   top: 100px;
   left: 0;
@@ -24,6 +25,7 @@ const StyledContainer = styled.section`
 export default ({ children }) => {
   const containerRef = useRef(null);
   const scrollRef = useRef(null);
+  const [ready, setReady] = useState(false);
   const mq =
     typeof window !== `undefined`
       ? window.matchMedia("(max-width: 1024px)")
@@ -45,14 +47,18 @@ export default ({ children }) => {
     }
     document.body.style.height = scrollHeight;
   };
-
   setInterval(() => {
     setBodyHeight();
   }, 500);
 
   useEffect(() => {
+    gsap.fromTo(
+      containerRef.current,
+      { autoAlpha: 0 },
+      { autoAlpha: 1, duration: 2 }
+    );
     setBodyHeight();
-  });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   useScrollPosition(
     ({ currPos }) => {
