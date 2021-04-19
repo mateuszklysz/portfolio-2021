8478f219fdf010c1f79f16bdd11ca943d4603aa2
Mateusz Kłysz <mateuszklysz@outlook.com>
2021-04-19 17:32:53 +0200

    update info page

--- src/components/About/Info/Info.js
+++ src/components/About/Info/Info.js
@@ -5,12 +5,14 @@ const InfoSection = () => (
   <Container>
     <Text>
       Programowaniem zajmuję się od małego. Od zawsze ciekawiły mnie produkcje
-      gier komputerowych, dlatego też szybko zainteresowałem się językiem{" "}
-      <span style={{ color: "#4f83cc" }}>C++</span>.
+      gier komputerowych, dlatego też szybko zainteresowałem się językami{" "}
+      <span style={{ color: "#0d47a1" }}>C++</span> oraz{" "}
+      <span style={{ color: "#7c43bd" }}>C#</span>.
       <br /> <br />
       Wstęp do <span style={{ color: "#ffb04c" }}>JavaScript</span> był dzięki
-      temu bardzo płynny i prosty, a nauka nowych technologii ciekawa i
-      nieprzytłaczająca.
+      temu bardzo płynny, a sam{" "}
+      <span style={{ color: "#5472d3" }}>Typescript</span> nie był aż tak ciężki
+      do nauki.
     </Text>
     <StyledCode />
   </Container>
