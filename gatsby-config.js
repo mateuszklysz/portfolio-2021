01dcc2678aca10b5c20ef12ac39d750531b449b7
Mateusz Kłysz <mateuszklysz@outlook.com>
2021-04-23 23:50:46 +0200

    add fixed sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
         path: `${__dirname}/src/templates/`,
       },
     },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-mdx`,
     `gatsby-plugin-image`,
     "gatsby-transformer-sharp",
--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -26,12 +26,6 @@ const SEO = () => (
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1, viewport-fit=cover"
         />
-        <meta
-          http-equiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta http-equiv="Pragma" content="no-cache" />
-        <meta http-equiv="Expires" content="0" />
         <meta name="description" content={description} />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Open+Sans:wght@400;700&display=swap"
