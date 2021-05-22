1492ca2d47b54144d0aa982f92c756c01fb0d50b
Mateusz Kłysz <mateuszklysz@outlook.com>
2021-05-22 22:32:46 +0200

    add google preconnect

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -27,6 +27,7 @@ const SEO = () => (
           content="width=device-width, initial-scale=1, minimum-scale=1, viewport-fit=cover"
         />
         <meta name="description" content={description} />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Open+Sans:wght@400;700&display=swap"
           rel="stylesheet"
